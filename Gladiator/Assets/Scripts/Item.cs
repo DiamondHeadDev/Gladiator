504210fe833641e7972fdbde2167f8e3e5ca7e71
Brandon Hale <bthaleproductions@gmail.com>
2020-05-09 18:23:59 -0400

    Made Scriptable object item for inventory. Made Item Helmet. Need to work on taking Items as scriptable objects and putting them on the inventory prefab

--- Gladiator/Assets/Scripts/Item.cs
+++ Gladiator/Assets/Scripts/Item.cs
@@ -1,23 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Item : MonoBehaviour
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
 {
+    public new string name;
+    public string description;
 
-    string name;
-    
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Sprite artwork;
 }
