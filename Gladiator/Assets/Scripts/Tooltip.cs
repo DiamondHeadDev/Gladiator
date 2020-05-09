61b5d03b8fa075b4bf556c863f3179ab4b2fa0d6
DiamondHeadDEv <denali95+1@vt.edu>
2020-05-09 09:40:49 -0400

    adding gui scripts

---
+++ Gladiator/Assets/Scripts/Tooltip.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler/// required interface when using the OnPointerEnter method.
+{
+    public GameObject test;
+    //Do this when the cursor enters the rect area of this selectable UI object.
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        test.SetActive(true);
+        Debug.Log("The cursor entered the selectable UI element.");
+    }
+
+    //Do this when the cursor exits the rect area of this selectable UI object.
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        test.SetActive(false);
+        Debug.Log("The cursor exited the selectable UI element.");
+    }
+}
