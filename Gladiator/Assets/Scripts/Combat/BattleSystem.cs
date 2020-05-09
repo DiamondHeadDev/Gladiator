891dfb95b08e4d7ab6941cd630c336644802230d
DiamondHeadDEv <denali95+1@vt.edu>
2020-05-09 12:16:26 -0400

    adding battlesystem

---
+++ Gladiator/Assets/Scripts/Combat/BattleSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject enemyPrefab;
+
+    public BattleState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+    }
+
+    IEnumerator SetupBattle()
+    {
+        yield return new WaitForSeconds(1f);
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        //Damage the enemy
+        yield return new WaitForSeconds(1f);
+
+        //check if enemy dead
+        //change state based on what happened
+    }
+
+    void PlayerTurn()
+    {
+        
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+
+        StartCoroutine(PlayerAttack());
+    }
+}
